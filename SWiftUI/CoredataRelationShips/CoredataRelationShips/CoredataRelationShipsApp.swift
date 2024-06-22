55124b789e25d90d364718e22c725f1c5acbf757
Ahmed <ntgclarity@NTGs-MacBook-Pro.local>
2024-06-22 15:14:45 +0300

    wwww

---
+++ SWiftUI/CoredataRelationShips/CoredataRelationShips/CoredataRelationShipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoredataRelationShipsApp.swift
+//  CoredataRelationShips
+//
+//  Created by NTG Clarity on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoredataRelationShipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
